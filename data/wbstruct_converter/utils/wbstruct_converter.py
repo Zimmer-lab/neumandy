03999a89e4c30a28bbd3eb9c2c9f2670608af29c
lianaak <lianaakobian@gmail.com>
2023-10-24 22:28:33 +0200

    finish wbstruct_converter script

--- data/wbstruct_converter/utils/wbstruct_converter.py
+++ data/wbstruct_converter/utils/wbstruct_converter.py
@@ -1,4 +1,7 @@
 import sys
+import wbstruct_to_dicts as wbstruct_dictioniaries
+import wbstruct_dicts_to_dataframes as wbstruct_dataframes
+
 
 directory_path = sys.argv[1]
 target_file = sys.argv[2]
@@ -11,5 +14,9 @@ save_as_hdf5 = sys.argv[7]
 if directory_path is None or not directory_path.strip():
     raise ValueError('No Directory Path provided')
 
-# printing all arguments
-# print(f"Argument 1: {directory_path}")
+datasets = wbstruct_dictioniaries.get_datasets_dict(directory_path,target_file,include,exclude,recording_type,simple) 
+
+dataframes = wbstruct_dataframes.get_dataframes(datasets, recording_type, save_as_hdf5)
+
+print("Available Recordings:",list(dataframes.keys()))
+dataframes['Dataset1_20190125_ZIM1428_Ctrl_w2'].head()
